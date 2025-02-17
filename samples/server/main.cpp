379f69942c70c5825106d23d64a930deadc65c61
Enzo <enzo.mariant@gmail.com>
2025-02-17 15:51:26 -0500

    Commencement du taf

--- samples/server/main.cpp
+++ samples/server/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-
+#include <map>
 #include <falcon.h>
-
+#include <thread>
 #include "spdlog/spdlog.h"
+
+std::map <std::string, uint64_t> mapAddresses;
 
 int main()
 {
@@ -25,3 +27,38 @@ int main()
     falcon->SendTo(ip, port, std::span {buffer.data(), static_cast<unsigned long>(recv_size)});
     return EXIT_SUCCESS;
 }
+
+void Listen(uint16_t port) {
+    //possiblement faire un fork en while true ?
+
+    auto falcon = Falcon::Listen("127.0.0.1", port);
+    while (true) {
+        std::string from_ip;
+        from_ip.resize(255);
+        std::array<char, 65535> buffer;
+
+        int recv_size = falcon->ReceiveFrom(from_ip, buffer);
+
+        std::thread([falcon, from_ip, buffer, recv_size, port]() {
+
+            std::string ip = from_ip;
+
+            auto pos = from_ip.find_last_of (':');
+            if (pos != std::string::npos) {
+                ip = from_ip.substr (0,pos);
+            }
+            
+            if (mapAddresses.find(ip) == mapAddresses.end()) {
+                mapAddresses[ip] = port;
+                //jouer onclientconnected en envoyant au client un message (
+            }
+
+        }).detach();
+    }
+}
+
+void OnClientConnected(std::function<void(uint64_t)> handler) {
+
+}
+
+void OnClientDisconnected(std::function<void, uint64_t> handler); //Server API
