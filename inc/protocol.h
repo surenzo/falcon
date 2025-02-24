b66ccb2a6ce6792a439c0d9c22f220f9b5bc1672
Enzo <enzo.mariant@gmail.com>
2025-02-24 00:20:03 -0500

    lefameu

--- inc/protocol.h
+++ inc/protocol.h
@@ -1,9 +1,11 @@
+// protocol.h
 #pragma once
 
+#include <cstdint>
+
 enum class ProtocolType : uint8_t {
-    Connect = 0x00,     // Demande de connexion
-    //Acknowledgement = 0x01, // Confirmation de réception
-    Stream = 0x03,       // Transmission de données en flux
-    Ping = 0xFF,
-    Pong = 0xFE
+    Connect,
+    Stream,
+    Ping,
+    Pong
 };
